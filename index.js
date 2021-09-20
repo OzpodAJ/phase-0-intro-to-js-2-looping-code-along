441b5391766fd12f8b2105f7011fcfeeadb9cc46
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-20 14:02:24 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let names = [];
+let thankYou = [];
+function writeCards(names, event){
+    for (let x = 0; x < names.length; x++) {
+        thankYou.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    console.log(thankYou)
+    return thankYou
+}
+writeCards([], 'surprise')
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n--)
+    }
+}
